33515cdc78e7ab8437d22a693127f4c4389247d5
Anton Gundermann <anton.gundermann@gmx.de>
2022-12-27 15:26:05 +0100

    started day15

---
+++ day15.py
@@ -0,0 +1,12 @@
+def main():
+    with open('day14.in') as f:
+        lines = [line.strip() for line in f]
+
+    print(solve(lines))
+
+
+def solve(lines):
+    pass
+
+if __name__ == '__main__':
+    main()
