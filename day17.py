e53a21a4c76e9e8023ae83546fe1d67736d68b36
Anton Gundermann <anton.gundermann@gmx.de>
2022-12-28 11:41:49 +0100

    refactor day 17

--- day17.py
+++ day17.py
@@ -1,6 +1,25 @@
+from itertools import combinations
+
+
 def main():
-    pass
-   
+    with open('day17.in') as f:
+        containers = [int(line) for line in f]
+    print(solve(containers))
+
+
+def solve(containers):
+    part1 = part2 = 0
+    for i in range(len(containers)):
+        if not any(sum(comb) == 150 for comb in combinations(containers, i)):
+            continue
+        for comb in combinations(containers, i):
+            if sum(comb) == 150:
+                part1 += 1
+                if i == 4:
+                    part2 += 1
+
+    return part1, part2
+
 
 if __name__ == '__main__':
     main()
